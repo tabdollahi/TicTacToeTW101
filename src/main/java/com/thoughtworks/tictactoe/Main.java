266cd9de050a6c5675c5dbd0421402aec736b6db
Bill Schofield <bill@billschofield.net>
2015-08-06 13:40:17 -0500

    TTT Gradle Template Project

---
+++ src/main/java/com/thoughtworks/tictactoe/Main.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Tic Tac Toe");
+    }
+}
---
+++ src/test/java/com/thoughtworks/tictactoe/MainTest.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.tictactoe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void shouldBeADummyTest() {
+
+    }
+
+}
