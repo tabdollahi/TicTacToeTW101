4247e5fe516ef5c40dad334158e5d8de82121865
tabdollahi <abdollahi.tania@gmail.com>
2016-01-14 17:20:21 -0600

    Fix board to have two spaces for each spot, instead of one. Add more tests to board

--- src/main/java/com/thoughtworks/tictactoe/Board.java
+++ src/main/java/com/thoughtworks/tictactoe/Board.java
@@ -5,7 +5,7 @@ import java.io.PrintStream;
 public class Board {
 
     private PrintStream printStream;
-    private char[][] board = new char [3][3];
+    private char[][] board = new char [3][6];
 
     public Board(PrintStream printStream) {
         this.printStream = printStream;
@@ -13,11 +13,11 @@ public class Board {
     }
 
     public void draw() {
-        printStream.println(board[0][0]+ "|" + board[0][1] + "|" + board[0][2]);
-        printStream.println("-----");
-        printStream.println(board[1][0]+ "|" + board[1][1] + "|" + board[1][2]);
-        printStream.println("-----");
-        printStream.println(board[2][0]+ "|" + board[2][1] + "|" + board[2][2]);
+        printStream.println(""+ board[0][0] + board[0][1] + "|" + board[0][2] + board[0][3]+ "|" + board[0][4] + board[0][5]);
+        printStream.println("--------");
+        printStream.println(""+ board[1][0] + board[1][1] + "|" + board[1][2] + board[1][3]+ "|" + board[1][4] + board[1][5]);
+        printStream.println("--------");
+        printStream.println(""+ board[2][0] + board[2][1] + "|" + board[2][2] + board[2][3]+ "|" + board[2][4] + board[2][5]);
         printStream.println();
 
     }
@@ -28,30 +28,28 @@ public class Board {
             board[0][0] = marker;
 
         } else if (playerMoveSelection.equals("2")) {
-            board[0][1] = marker;
-
             board[0][2] = marker;
 
         } else if (playerMoveSelection.equals("3")) {
-            board[0][2] = marker;
+            board[0][4] = marker;
 
         } else if (playerMoveSelection.equals("4")) {
             board[1][0] = marker;
 
         } else if (playerMoveSelection.equals("5")) {
-            board[1][1] = marker;
+            board[1][2] = marker;
 
         } else if (playerMoveSelection.equals("6")) {
-            board[1][2] = marker;
+            board[1][4] = marker;
 
         } else if (playerMoveSelection.equals("7")) {
             board[2][0] = marker;
 
         } else if (playerMoveSelection.equals("8")) {
-            board[2][1] = marker;
+            board[2][2] = marker;
 
         } else if (playerMoveSelection.equals("9")) {
-            board[2][2] = marker;
+            board[2][4] = marker;
         }
         draw();
     }
@@ -59,7 +57,7 @@ public class Board {
     private void initialBoardSetup(){
         for(int i = 0; i < 3; i++)
         {
-            for(int j = 0; j <3; j++)
+            for(int j = 0; j <6; j++)
             {
                 board[i][j] = ' ';
             }
--- src/test/java/com/thoughtworks/tictactoe/BoardTest.java
+++ src/test/java/com/thoughtworks/tictactoe/BoardTest.java
@@ -25,23 +25,129 @@ public class BoardTest {
 
         InOrder inOrder = Mockito.inOrder(printStream);
 
-        inOrder.verify(printStream).println(" | | ");
-        inOrder.verify(printStream).println("-----");
-        inOrder.verify(printStream).println(" | | ");
-        inOrder.verify(printStream).println("-----");
-        inOrder.verify(printStream).println(" | | ");
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
     }
 
     @Test
-    public void shouldReDrawBoardWhenUserMakesANewMove() {
+    public void shouldDrawXInUpperLeftCornerIfPlayer1SelectionIs1() {
         board.redraw("1");
 
         InOrder inOrder = Mockito.inOrder(printStream);
 
-        inOrder.verify(printStream).println("X| | ");
-        inOrder.verify(printStream).println("-----");
-        inOrder.verify(printStream).println(" | | ");
-        inOrder.verify(printStream).println("-----");
-        inOrder.verify(printStream).println(" | | ");
+        inOrder.verify(printStream).println("X |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
     }
+
+    @Test
+    public void shouldDrawXInUpperMiddleIfPlayer1SelectionIs2() {
+        board.redraw("2");
+
+        InOrder inOrder = Mockito.inOrder(printStream);
+
+        inOrder.verify(printStream).println("  |X |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+    }
+
+    @Test
+    public void shouldDrawXInUpperRightIfPlayer1SelectionIs3() {
+        board.redraw("3");
+
+        InOrder inOrder = Mockito.inOrder(printStream);
+
+        inOrder.verify(printStream).println("  |  |X ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+    }
+
+    @Test
+    public void shouldDrawXInMiddleLeftIfPlayer1SelectionIs4() {
+        board.redraw("4");
+
+        InOrder inOrder = Mockito.inOrder(printStream);
+
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("X |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+    }
+
+    @Test
+    public void shouldDrawXInCenterIfPlayer1SelectionIs5() {
+        board.redraw("5");
+
+        InOrder inOrder = Mockito.inOrder(printStream);
+
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |X |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+    }
+
+    @Test
+    public void shouldDrawXInMiddleRightIfPlayer1SelectionIs6() {
+        board.redraw("6");
+
+        InOrder inOrder = Mockito.inOrder(printStream);
+
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |X ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+    }
+
+    @Test
+    public void shouldDrawXInBottomLeftIfPlayer1SelectionIs7() {
+        board.redraw("7");
+
+        InOrder inOrder = Mockito.inOrder(printStream);
+
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("X |  |  ");
+    }
+
+    @Test
+    public void shouldDrawXInBottomMiddleIfPlayer1SelectionIs8() {
+        board.redraw("8");
+
+        InOrder inOrder = Mockito.inOrder(printStream);
+
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |X |  ");
+    }
+
+    @Test
+    public void shouldDrawXInBottomRightIfPlayer1SelectionIs9() {
+        board.redraw("9");
+
+        InOrder inOrder = Mockito.inOrder(printStream);
+
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |  ");
+        inOrder.verify(printStream).println("--------");
+        inOrder.verify(printStream).println("  |  |X ");
+    }
+
+
 }
